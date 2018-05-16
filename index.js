8170b0d35112c64f8e02abc3bc77d0a80fd74fe4
sean-blattenberger <sean.blattenberger@gmail.com>
2018-05-16 15:27:37 -0600

    Added new array

---
+++ index.js
@@ -0,0 +1,29 @@
+const arrayOfObjects = [
+    {
+      name: "Sean",
+      age: "28"
+    },
+    {
+      name: "Adrian",
+      age: "33"
+    },
+    {
+      name: "Alex",
+      age: "19"
+    }
+];
+
+const arrayOfObjects2 = [
+    {
+      name: "Sean",
+      age: "28"
+    },
+    {
+      name: "Adrian",
+      age: "33"
+    },
+    {
+      name: "Alex",
+      age: "19"
+    }
+];
